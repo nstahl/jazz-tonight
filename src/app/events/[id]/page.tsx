be09745f00d6ed4a91c28f34b1d592393316f64c
niko <r.niko.stahl@gmail.com>
2025-03-08 14:18:21 -0500

    added post interface

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,9 +1,14 @@
+interface Post {
+  id: string;
+  title: string;
+}
+
 export default async function Page() {
     const data = await fetch('https://api.vercel.app/blog')
     const posts = await data.json()
     return (
       <ul>
-        {posts.map((post) => (
+        {posts.map((post: Post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
