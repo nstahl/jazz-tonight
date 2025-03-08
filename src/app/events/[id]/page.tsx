b7f0313fe71dffb9c605d44b06dc82f8993e9997
niko <r.niko.stahl@gmail.com>
2025-03-08 13:51:10 -0500

    basic data fetch

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <h1>This is an events detail page</h1>
-}
+export default async function Page() {
+    const data = await fetch('https://api.vercel.app/blog')
+    const posts = await data.json()
+    return (
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    )
+  }
