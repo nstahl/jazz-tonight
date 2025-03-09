556910f5a7555026fd413b3abb18e811b01a193f
niko <r.niko.stahl@gmail.com>
2025-03-09 18:59:20 -0400

    some styling experiments

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -17,10 +17,15 @@ export default async function Page() {
     console.log(events);
     
     return (
-      <ul>
-        {events.map((event: Event) => (
-          <li key={event.id}>{event.name}</li>
-        ))}
-      </ul>
+      <div className="flex flex-col items-center py-10">
+        <h1 className="text-2xl font-bold">Events</h1>
+        <div className="list-inside list-disc pl-4 pt-8 mx-auto max-w-2xl w-full px-6">
+          {events.map((event: Event) => (
+            <div key={event.id} className="ml-4 py-16 px-6 my-4">
+              {event.name}
+            </div>
+          ))}
+        </div>
+      </div>
     )
   }
