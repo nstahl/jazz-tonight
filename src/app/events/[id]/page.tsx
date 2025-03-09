5e7ce7fce89850a79bf1af9b92978a7cdc733b3d
niko <r.niko.stahl@gmail.com>
2025-03-09 08:30:40 -0400

    returning prisma data entries

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,11 +1,18 @@
+import prisma from '../../../../lib/prisma';
+
 interface Post {
   id: string;
   title: string;
 }
 
 export default async function Page() {
-    const data = await fetch('https://api.vercel.app/blog')
-    const posts = await data.json()
+    // const data = await fetch('https://api.vercel.app/blog')
+    // const posts = await data.json()
+
+    const posts = await prisma.post.findMany();
+
+    console.log(posts);
+    
     return (
       <ul>
         {posts.map((post: Post) => (
