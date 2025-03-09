4176547b5fb584ca6f676b4c8962b905facfcb43
niko <r.niko.stahl@gmail.com>
2025-03-09 08:44:55 -0400

    add event schema

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,22 +1,25 @@
 import prisma from '../../../../lib/prisma';
 
-interface Post {
+interface Event {
   id: string;
-  title: string;
+  name: string;
+  url: string;
+  dateTime: Date;
+  venueId: string;
 }
 
 export default async function Page() {
     // const data = await fetch('https://api.vercel.app/blog')
     // const posts = await data.json()
 
-    const posts = await prisma.post.findMany();
+    const events = await prisma.event.findMany();
 
-    console.log(posts);
+    console.log(events);
     
     return (
       <ul>
-        {posts.map((post: Post) => (
-          <li key={post.id}>{post.title}</li>
+        {events.map((event: Event) => (
+          <li key={event.id}>{event.name}</li>
         ))}
       </ul>
     )
