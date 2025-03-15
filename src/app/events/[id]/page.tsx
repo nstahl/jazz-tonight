39878166c2f5b9ff955dcc95dbbdc9fb4553060e
niko <r.niko.stahl@gmail.com>
2025-03-15 16:48:57 -0400

    Clean up CSS and structure page

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,31 +1,8 @@
-import prisma from '../../../../lib/prisma';
-
-interface Event {
-  id: string;
-  name: string;
-  url: string;
-  dateTime: Date;
-  venueId: string;
-}
-
 export default async function Page() {
-    // const data = await fetch('https://api.vercel.app/blog')
-    // const posts = await data.json()
-
-    const events = await prisma.event.findMany();
-
-    console.log(events);
     
     return (
-      <div className="flex flex-col items-center py-10">
-        <h1 className="text-2xl font-bold">Events</h1>
-        <div className="list-inside list-disc pl-4 pt-8 mx-auto max-w-2xl w-full px-6">
-          {events.map((event: Event) => (
-            <div key={event.id} className="ml-4 py-16 px-6 my-4">
-              {event.name}
-            </div>
-          ))}
-        </div>
+      <div>
+        <h1>Event</h1>
       </div>
     )
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,46 @@
-export default function Page() {
-  return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+import prisma from '../../lib/prisma';
+
+interface Event {
+  id: string;
+  name: string;
+  url: string;
+  dateTime: Date;
+  venueId: string;
+  venue: {
+    name: string;
+  };
 }
+
+export default async function Page() {
+    const events = await prisma.event.findMany({
+      include: {
+        venue: true
+      }
+    });
+    
+    return (
+      <div>
+        <h1 className="text-2xl font-bold">Events</h1>
+        <div>
+          {events.map((event: Event) => (
+            <div key={event.id}>
+              <span>{event.name}</span>
+              {' | '}
+              <span>{event.dateTime.toLocaleDateString()}</span>
+              {' | '}
+              <span>{event.venue.name}</span>
+              {' | '}
+              <a 
+                className="text-blue-500 visited:text-purple-500" 
+                href={event.url} 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                View
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
