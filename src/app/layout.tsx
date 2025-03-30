41b7feafa7a52e6de62d5dfd4431ea845f352c21
niko <r.niko.stahl@gmail.com>
2025-03-30 15:42:32 -0400

    fix import statement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import Analytics from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/react'
  
 // These styles apply to every route in the application
 import './globals.css'
