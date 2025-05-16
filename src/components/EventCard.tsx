9f2d2ec8ed0526f44af8f4609906455aa05dcee8
niko <r.niko.stahl@gmail.com>
2025-05-16 17:07:18 -0400

    remove links on card

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -186,13 +186,7 @@ function EventCard({ event, id }) {
       )}
       {/* Event Name */}
       <div className={`text-xl font-bold mb-2 text-white ${fugazOne.className}`}>
-        {event.artist ? (
-          <a href={`/artist/${event.artist.id}`} className="hover:underline">
-            {event.name}
-          </a>
-        ) : (
           <span>{event.name}</span>
-        )}
       </div>
       {/* Date, Times, Venue */}
       <div className="flex justify-between text-sm text-zinc-400 mb-2">
@@ -232,54 +226,16 @@ function EventCard({ event, id }) {
           {event.venue.logline}
         </div>
       )}
-      <div className="grid grid-cols-[24px_1fr] gap-x-2 gap-y-4 mt-2">
+      <div className="flex flex-col gap-y-4 mt-2">
         {/* Event Logline */}
         {event.logline && (
           <>
-            <svg className="w-4 h-4 mt-1 text-zinc-400" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/>
-            </svg>
-            <div className="text-sm text-zinc-200">
-              <h3 className="font-semibold mb-1">What to expect</h3>
+            <div className="text-md text-zinc-200">
               <p>{event.logline}</p>
             </div>
           </>
         )}
-        {/* Musicians List */}
-        {event.performers && event.performers.length > 0 && (
-          <>
-            <svg className="w-4 h-4 mt-1 text-zinc-400" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5z"/>
-            </svg>
-            <div className="text-sm text-zinc-200">
-              <h3 className="font-semibold mb-1">The Band</h3>
-              <div className="inline-grid grid-cols-2 gap-x-1 gap-y-1">
-                {event.performers.map((m) => (
-                  <React.Fragment key={event.id + '-' + m.performer.id}>
-                    <div className="flex">
-                      <span className="text-zinc-400">{m.performer.instrument}</span>
-                    </div>
-                    <div className="flex">
-                      <span>{m.performer.name}</span>
-                    </div>
-                  </React.Fragment>
-                ))}
-              </div>
-            </div>
-          </>
-        )}
-        {/* Venue Info */}
-        {event.venue && event.venue.name && (
-          <>
-            <svg className="w-4 h-4 mt-1 text-zinc-400" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/>
-            </svg>
-            <div className="text-sm text-zinc-200">
-              <h3 className="font-semibold mb-1">About {event.venue.name}</h3>
-              <p>More info on the venue coming soon...</p>
-            </div>
-          </>
-        )}
+
       </div>
     </div>
   );
