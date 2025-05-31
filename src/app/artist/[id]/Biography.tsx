b4c2dc67d35d621cb7e285d5751cea48afba928e
niko <r.niko.stahl@gmail.com>
2025-05-31 17:27:15 -0400

    remove show less/more in bio

--- src/app/artist/[id]/Biography.tsx
+++ src/app/artist/[id]/Biography.tsx
@@ -7,7 +7,7 @@ type BiographyProps = {
 };
 
 export default function Biography({ text }: BiographyProps) {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(true);
   
   // If text is short enough, don't show the expand/collapse functionality
   if (text.length <= 300) {
@@ -23,19 +23,10 @@ export default function Biography({ text }: BiographyProps) {
     <div className="mb-6">
       <h3 className="text-xl font-semibold mb-2">About</h3>
       <div className="relative">
-        <p className={`text-gray-100 ${!isExpanded ? 'line-clamp-6' : ''}`}>
+        <p className={`text-gray-100`}>
           {text}
         </p>
-        {!isExpanded && (
-          <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-black to-transparent" />
-        )}
       </div>
-      <button
-        onClick={() => setIsExpanded(!isExpanded)}
-        className="text-blue-300 hover:underline mt-2 cursor-pointer"
-      >
-        {isExpanded ? 'Show less' : 'Show more'}
-      </button>
     </div>
   );
 } 
